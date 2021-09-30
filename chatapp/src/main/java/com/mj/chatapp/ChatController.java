0a3868cc928a697cc4502f9d369cda65c2250594
minjoon_lee <minjoon1995@naver.com>
2021-09-30 13:35:21 +0900

    first commit

---
+++ chatapp/src/main/java/com/mj/chatapp/ChatController.java
@@ -0,0 +1,30 @@
+package com.mj.chatapp;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@RestController
+public class ChatController {
+    private final ChatRepository chatRepository;
+    //sse 프로토콜
+    @GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver) {
+        return chatRepository.mFindBySender(sender, receiver)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @PostMapping("/chat")
+    public Mono<Chat> setMsg(@RequestBody Chat chat) {
+        chat.setCreatedAt(LocalDateTime.now());
+        return chatRepository.save(chat);
+    }
+
+}
---
+++ chatapp/src/main/java/com/mj/chatapp/ChatRepository.java
@@ -0,0 +1,12 @@
+package com.mj.chatapp;
+
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
+import reactor.core.publisher.Flux;
+
+public interface ChatRepository extends ReactiveMongoRepository<Chat, String> {
+    @Tailable //커서를 안 닫고 계속 유지한다.
+    @Query("{sender:?0,receiver:?1}")
+    Flux<Chat> mFindBySender(String sender, String receiver);
+}
